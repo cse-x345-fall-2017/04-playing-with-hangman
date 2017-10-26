29009d5eb7696f94a0fe672ac14aadb261ac2c33
Gabo <gleyvamerino@smu.edu>
2017-10-26 13:51:43 -0500

    Testing Syntax

--- dictionary/lib/dictionary/agent.ex
+++ dictionary/lib/dictionary/agent.ex
@@ -1,15 +1,14 @@
 defmodule Dictionary.Agent
 do
 
-alias Dictionary.WordList
 @name :Agent_Dave
 
   def start_link() do
-    Agent.start_link(&WordList.word_list/0, name: @name)
+    Agent.start_link(&Dictionary.WordList.word_list/0, name: @name)
   end
 
   def random_word() do
-    Agent.get(@name, &WordList.random_word/1)
+    Agent.get(@name, &Dictionary.WordList.random_word/1)
   end
 
 end

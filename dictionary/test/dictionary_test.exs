2ee01c2eb9cb5f311a72c0bdfc086a755aa06e99
Luke Wood <lukewoodcs@gmail.com>
2017-10-26 13:34:50 -0500

    Starting?

--- dictionary/test/dictionary_test.exs
+++ dictionary/test/dictionary_test.exs
@@ -3,6 +3,7 @@ defmodule DictionaryTest do
   doctest Dictionary
 
   test "Words are returned" do
+    Dictionary.start("", "")
     word1 = Dictionary.random_word()
     assert word1 =~ ~r/^[a-z']+$/
   end

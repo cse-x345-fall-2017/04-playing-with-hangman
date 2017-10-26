0ef5b829bf5370490dbbe990d47f81e6a1ff3d9c
Luke Wood <lukewoodcs@gmail.com>
2017-10-26 13:43:06 -0500

    Reverted test and moved to mix.exs

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,9 +1,10 @@
 defmodule Dictionary do
 
   use Application
-
   defdelegate random_word(), to: Dictionary.Agent
 
+  # TODO(lukewood) I should make start take a name arg
+  #                and pass it down the chain.
   def start(_type, _args) do
     Dictionary.Supervisor.start_link()
   end

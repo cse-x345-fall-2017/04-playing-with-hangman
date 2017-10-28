a68911278c655dd98f6d5e8c41cc72b1ce4269df
Luke Wood <lukewoodcs@gmail.com>
2017-10-28 15:05:14 -0500

    it had to be a list.;..

--- hangman/lib/hangman/client.ex
+++ hangman/lib/hangman/client.ex
@@ -2,4 +2,15 @@ defmodule Hangman.Client do
   def new_game() do
     GenServer.call(Hangman.Server, {:make_move})
   end
+  def new_game(word) do
+    GenServer.call(Hangman.Server, {:make_move, word})
+  end
+
+  def tally(game) do
+    GenServer.call(Hangman.Server, {:tally, game})
+  end
+
+  def make_move(game, guess) do
+    GenServer.call(Hangman.Server, {:make_move, game, guess})
+  end
 end
--- hangman/mix.exs
+++ hangman/mix.exs
@@ -17,7 +17,7 @@ defmodule Hangman.Mixfile do
       extra_applications: [
         :logger
       ],
-      mod: { Hangman.Supervisor, [] }
+      mod: { Hangman.Application, [] }
     ]
   end
 

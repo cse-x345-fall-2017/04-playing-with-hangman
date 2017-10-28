5ff180b5aa3989b0316aa05a86848919b624753a
Luke Wood <lukewoodcs@gmail.com>
2017-10-28 14:59:12 -0500

    store update

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,3 +1,13 @@
 defmodule Hangman do
   alias Hangman.Client, as: Client
+
+  defdelegate new_game(),       to: Client
+  defdelegate new_game(word),   to: Client
+  defdelegate tally(game),      to: Client
+  defdelegate make_move(game, guess),       to: Client
+
+  def start_link do
+    GenServer.start_link(Hangman.Server, [], name: __MODULE__)
+  end
+
 end

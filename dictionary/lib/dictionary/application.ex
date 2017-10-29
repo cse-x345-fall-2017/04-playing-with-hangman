3e815f404e347bad3e2c1881298aea88bd0262cf
ganktoby <zhanglijietoby@gmail.com>
2017-10-28 22:47:52 -0500

    fogot to add application in dictionary

--- dictionary/lib/dictionary/application.ex
+++ dictionary/lib/dictionary/application.ex
@@ -2,12 +2,12 @@ defmodule Dictionary.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec. warn: false
+    import Supervisor.Spec, warn: false
     children = [
       worker(Dictionary.Agent, [])
     ]
     opts = [
-      startegy: :one_for_one
+      strategy: :one_for_one,
       name: Dictionary.Supervisor
     ]
     Supervisor.start_link(children, opts)

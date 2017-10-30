46c97c829b00e2904f04b20024819b26b3218109
KNayak430 <ksnayak@smu.edu>
2017-10-29 20:01:58 -0500

    Assignment 4 Submission

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -1,10 +1,16 @@
 defmodule Dictionary.WordList do
 
+  @name :word_agent
+
+  def start_link() do
+    Agent.start_link(fn -> word_list() end, name: @name)
+  end
+
   def random_word() do
-    word_list()
+    Agent.get(@name, fn state -> state end)
     |> Enum.random()
   end
-  
+
   def word_list do
     "../../assets/words.txt"
     |> Path.expand(__DIR__)
